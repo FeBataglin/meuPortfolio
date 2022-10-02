ec42e360fbbb2a05742165ad50acd546fd493b1b
Fernando <fernando.bataglin@hotmail.com>
2022-10-02 05:27:48 -0300

    finalização tela inicial

--- src/assets/js/navigation.js
+++ src/assets/js/navigation.js
@@ -5,8 +5,8 @@ function navigationWithClick() {
     var button_four = document.getElementById('bt-four');
 
     button_one.onclick = function (e) {
-        button_one.classList.remove('navigation-one');
-        button_one.classList.add('navigation-one-active');
+        button_one.classList.remove('navigation-one-active');
+        button_one.classList.add('navigation-one');
 
         button_two.classList.remove('navigation-two-active');
         button_two.classList.add('navigation-two');
@@ -22,20 +22,20 @@ function navigationWithClick() {
         button_two.classList.remove('navigation-two');
         button_two.classList.add('navigation-two-active');
 
-        button_one.classList.remove('navigation-one-active');
+        /*button_one.classList.remove('navigation-one-active');
         button_one.classList.add('navigation-one');
 
         button_three.classList.remove('navigation-three-active');
-        button_three.classList.add('navigation-three');
+        button_three.classList.add('navigation-three-');
 
         button_four.classList.remove('navigation-four-active');
-        button_four.classList.add('navigation-four');
+        button_four.classList.add('navigation-four');*/
     }
 
     button_three.onclick = function (e) {
         button_three.classList.remove('navigation-three');
         button_three.classList.add('navigation-three-active');
-
+        
         button_two.classList.remove('navigation-two-active');
         button_two.classList.add('navigation-two');
 
@@ -50,14 +50,14 @@ function navigationWithClick() {
         button_four.classList.remove('navigation-four');
         button_four.classList.add('navigation-four-active');
 
-        button_two.classList.remove('navigation-two-active');
+        /*button_two.classList.remove('navigation-two-active');
         button_two.classList.add('navigation-two');
 
         button_three.classList.remove('navigation-three-active');
         button_three.classList.add('navigation-three');
 
         button_one.classList.remove('navigation-one-active');
-        button_one.classList.add('navigation-one');
+        button_one.classList.add('navigation-one');*/
     }
 }
 
